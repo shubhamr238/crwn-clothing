05524957b507449eca7a7ae8b91abbef1448c5b2
shubhamr238 <shubhamr238@gmail.com>
2020-04-15 16:14:28 +0530

    broken homepage component into diff other components, finished styling menu-item component (End of lesson-3)

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Directory from '../../components/directory/directory.component';
+
+
+import './homepage.style.scss';
+
+const Hompage=()=>(
+    <div className="homepage">
+        <Directory/>
+    </div>
+);
+
+export default Hompage;
