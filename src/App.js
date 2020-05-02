f533e324e2c7b8e623e170264fb79de44ad59d78
shubhamr238 <shubhamr238@gmail.com>
2020-05-02 14:37:49 +0530

    add custom-button comp, form-input comp, and sign-in page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 // const HatsPage=()=>(
 //   <div>
@@ -20,6 +21,7 @@ function App() {
     <Switch>
       <Route exact path='/' component={Homepage}/>
       <Route exact path='/shop' component={ShopPage}/>
+      <Route exact path='/signin' component={SignInAndSignUpPage}/>
     </Switch>
     </div>
   );
